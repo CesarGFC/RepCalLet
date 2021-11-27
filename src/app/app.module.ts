15320b55687ffb148fc54166a27547c5502e9ba2
César Flores Carrillo <cesarin_gfc@hotmail.es>
2021-11-27 12:05:46 -0700

    firt commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+// import module
+import { HttpClientModule } from '@angular/common/http';
+
+import { LoginPage } from './pages/login/login.page';
+import { HomePage } from './pages/home/home.page';
+import { RegistrationPage } from './pages/registration/registration.page';
+import { ForgotPasswordPage } from './pages/forgot-password/forgot-password.page';
+import { Tab1Page } from './pages/tab1/tab1.page';
+import { Tab2Page } from './pages/tab2/tab2.page';
+import { Tab3Page } from './pages/tab3/tab3.page';
+
+@NgModule({
+  declarations: [
+    AppComponent,   
+    HomePage,
+    RegistrationPage,
+    ForgotPasswordPage,
+    LoginPage,
+    Tab1Page,
+    Tab2Page,
+    Tab3Page],
+  entryComponents: [
+  ],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule],
+    providers: [{ provide: RouteReuseStrategy, 
+                useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
